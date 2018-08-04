ffa036b708b7692fda62594bdc67f96b73027505
Andrew <avgbyron@email.arizona.edu>
2018-08-03 20:26:32 -0700

    Added a test for logging with formatted strings, but really just testing Jenkins!

--- UnitTest.py
+++ UnitTest.py
@@ -15,9 +15,19 @@ class LoggerTestCase(unittest.TestCase):
         self.logger.stop()
         result = open('Test.log','r').read()        
         self.assertEqual(testString + '\n', result)
+    def test_log_complex(self):
+        int_ = 0xdeadbeef
+        float_ = 3.14159
+        string_ = 'This is a more complicated test'
+        testString = '{0} {1} {2}'.format(int_, float_, string_)
+        self.logger.log(testString)
+        self.logger.stop()
+        result = open('Test.log','r').read()        
+        self.assertEqual(testString + '\n', result)
     def suite():
         suite = unittest.TestSuite()
         suite.addTest(LoggerTestCase('test_log'))
+        suite.addTest(LoggerTestCase('test_log_complex'))
         return suite        
         
 if __name__ == '__main__':
